d2e9b582172691e0f00f08762f25bd10fcdfdb54
PhuDepTrai2001 <63588106+PhuDepTrai2001@users.noreply.github.com>
2021-03-29 20:18:12 +0700

    Seeding (#6)
    Co-authored-by: Trong Phu <phuqn10xxx@gmail.com>

--- Survey/Migrations/202103261343166_init.cs
+++ Survey/Migrations/202103261343166_init.cs
@@ -3,7 +3,7 @@ namespace Survey.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class Init : DbMigration
+    public partial class init : DbMigration
     {
         public override void Up()
         {
