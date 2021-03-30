7dc97477dd0b10f405e522ca69ba30e316a70813
trinhlh96 <trinhlhth1908021@fpt.edu.vn>
2021-03-30 23:12:13 +0700

    fix

--- Survey/Controllers/QuestionAnswersController.cs
+++ Survey/Controllers/QuestionAnswersController.cs
@@ -41,13 +41,13 @@ namespace Survey.Controllers
         public ActionResult Create()
         {
 
-            List<BigModel> ci = new List<BigModel> { new BigModel 
-            { 
+            List<BigModel> ci = new List<BigModel> { new BigModel
+            {
                 AllSurvey = new AllSurvey {
 
-                }, 
-                Question = new Question { 
-                }, 
+                },
+                Question = new Question {
+                },
                 QuestionAnswer = new QuestionAnswer {
 
                 } } };
@@ -99,15 +99,23 @@ namespace Survey.Controllers
                     // Lưu đáp án
                     for (var i = 0; i < bigModel.Count(); i++)
                     {
-                       
-                        if (bigModel[i].QuestionAnswer != null)
-                            {
-                                
-                                bigModel[i].QuestionAnswer.QuestionId = bigModel[i].Question.Id;
-                                db.Question_answers.Add(bigModel[i].QuestionAnswer);
 
+                        if (bigModel[i].QuestionAnswer != null)
+                        {
+
+                            if (i > 0)
+                            {
+                                if (bigModel[i].Question == null)
+                                {
+                                    bigModel[i].Question = bigModel[i - 1].Question;
+                                }
                             }
-                       
+
+                            bigModel[i].QuestionAnswer.QuestionId = bigModel[i].Question.Id;
+                            db.Question_answers.Add(bigModel[i].QuestionAnswer);
+
+                        }
+
                     }
                     db.SaveChanges();
 
