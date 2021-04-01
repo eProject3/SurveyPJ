0eeb79ab1f48572f3232547826c2711e9c69d8d6
xuanphuchb <41832648+XuanPhucHb@users.noreply.github.com>
2021-04-01 19:54:00 +0700

    ok (#9)
    Co-authored-by: phoavn96 <12345Bien>

--- Survey/Controllers/HomeController.cs
+++ Survey/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Survey.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +10,7 @@ namespace Survey.Controllers
 {
     public class HomeController : Controller
     {
+       private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
             return View();
@@ -26,5 +29,44 @@ namespace Survey.Controllers
 
             return View();
         }
+
+
+
+        public ActionResult SurveyDetails()
+        {
+            //var survey = db.Surveys.Where(p => p.SurveyId == id).ToList();
+            //List<Question> listQuestion = db.Questions.Where(p => p.SurveyId == id).ToList();
+
+            //foreach (Question q in listQuestion)
+            //{
+            //    List<QuestionAnswer> listQuestionAnswer = db.Question_answers.Where(p => p.QuestionId == q.Id).ToList();
+            //    q.QuestionAnswers = listQuestionAnswer;
+            //    ViewBag.Answer = listQuestionAnswer;
+            //};
+            //ViewBag.Question = listQuestion;
+            //ViewBag.Survey = survey;
+
+
+
+
+
+            return View(db.Surveys.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AllSurvey allSurvey = db.Surveys.Find(id);
+            if (allSurvey == null)
+            {
+                return HttpNotFound();
+            }
+            return View(allSurvey);
+        }
+
+
     }
 }
